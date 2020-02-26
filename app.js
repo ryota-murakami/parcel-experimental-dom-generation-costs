38986e774dd215c8c8962a1f326d7d0ec50cf563
ryota-murakami <dojce1048@gmail.com>
2020-02-27 01:58:41 +0900

    commit

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+window.addEventListener('DOMContentLoaded', (event) => {
+  function counterFunc(num) {
+
+    function innerFunc() {
+      return Date.now()
+    }
+
+    return num + 1
+  }
+
+  var setIntervalId
+  let num = 1
+  function fireCounter() {
+    setIntervalId = setInterval(() => {
+      const result = counterFunc(num++)
+      let li = document.createElement('li')
+      li.innerText = result
+      document.getElementById('list').appendChild(li)
+    }, 500)
+  }
+
+  fireCounter()
+
+  const StopButton = document.getElementById('stop-button')
+  StopButton.addEventListener('click', () => {
+    if (setIntervalId) {
+      clearInterval(setIntervalId)
+      console.log(setIntervalId)
+      setIntervalId = undefined
+      console.log(setIntervalId)
+    }
+  })
+
+  const StartButton = document.getElementById('start-button')
+  StartButton.addEventListener('click', () => setIntervalId || fireCounter())
+})
