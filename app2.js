398140e933e9d58eab09f01b3e546755878fe347
ryota-murakami <dojce1048@gmail.com>
2020-03-19 12:51:44 +0900

    to buton app2

--- app2.js
+++ app2.js
@@ -1,90 +1,97 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  let forEndFlag = false
-  for (let i=0;i<10000;i++) {
-    let div = document.createElement('div')
-    div.innerText = '<div>'
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
-    document.body.appendChild(div)
+window.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('start-button')
+  console.log(btn)
+  btn.addEventListener('click', (event) => {
+    let forEndFlag = false
+    for (let i=0;i<100;i++) {
+      let div = document.createElement('div')
+      div.innerText = '<div>'
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
+      document.body.appendChild(div)
 
-    if (i % 0) {
-      document.body.innerHTML = ''
+      if (i % 0) {
+        document.body.innerHTML = ''
+      }
+
+      if (i === (100000 - 1)) {
+        forEndFlag = true
+      }
     }
 
-    if (i === (100000 - 1)) {
-      forEndFlag = true
+    if (forEndFlag) {
+      document.body.innerText = 'End'
     }
-  }
 
-  if (forEndFlag) {
-    document.body.innerText = 'End'
-  }
-
+  })
 })
+
+
+
